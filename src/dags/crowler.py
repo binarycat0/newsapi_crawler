4a22d67056e0b48d68ecc9b47d55bc078058f180
Artur Rakhmatulin <arahmatulin@b2broker.net>
2019-08-29 02:23:03 +0300

    crolwer in the beginning

---
+++ src/dags/crowler.py
@@ -0,0 +1,34 @@
+import os
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.http_operator import SimpleHttpOperator
+from airflow.logging_config import log
+
+NEWSAPI_TOKEN = os.environ.get('NEWSAPI_TOKEN')
+log.debug(f'NEWSAPI_TOKEN: {NEWSAPI_TOKEN}')
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.now(),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG('newsapi_crowler', default_args=default_args, schedule_interval=timedelta(days=1))
+
+t1 = SimpleHttpOperator(
+    task_id='test_api',
+    dag=dag,
+    endpoint='/v2/everything/?q=bitcoin',
+    http_conn_id='newsapi',
+    headers={'Authorization': f'Bearer {NEWSAPI_TOKEN}'},
+    method='GET'
+)
