11ddf11fc212d5b17310ede3967de6209815bda3
Artur Rakhmatulin <arahmatulin@b2broker.net>
2019-09-04 17:01:21 +0300

    tasks base classes

--- dags/crawler.py
+++ dags/crawler.py
@@ -20,9 +20,14 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 
+    'method': 'GET',
+    'headers': {'Authorization': f'Bearer {NEWSAPI_TOKEN}'},
+    'http_conn_id': 'newsapi',
+    'endpoint': '/',
+    'log_response': False,
 }
 
-newsapi_crawler_dag = DAG(
+default_dag = DAG(
     'newsapi_crawler',
     default_args=default_args,
     schedule_interval=timedelta(days=1),
@@ -32,7 +37,43 @@ newsapi_crawler_dag = DAG(
 class NewsApiError(Exception): ...
 
 
-class PingNewsApiOperator(SimpleHttpOperator):
+class _SimpleHttpOperator(SimpleHttpOperator):
+    task_id = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(task_id=self.task_id, dag=default_dag, *args, **kwargs)
+
+
+class PublishEventsToPubSub(_SimpleHttpOperator):
+    task_id = 'publish_events_to_pub_sub'
+
+
+class LoadsDataToBigQuery(_SimpleHttpOperator):
+    task_id = 'loads_data_to_big_query'
+
+
+class StoreDataToGoogleDrive(_SimpleHttpOperator):
+    task_id = 'store_data_to_google_drive'
+
+
+class GetData(_SimpleHttpOperator):
+    task_id = 'get_data_api'
+
+    def execute(self, context):
+        execution_date = context.get("execution_date")
+        return super().execute(context)
+
+
+class CheckDataSchemas(_SimpleHttpOperator):
+    task_id = 'check_data_schemas'
+
+
+class GetDataSchemas(_SimpleHttpOperator):
+    task_id = 'get_data_schemas'
+
+
+class PingNewsApiOperator(_SimpleHttpOperator):
+    task_id = 'ping_news_api'
 
     def execute(self, context):
         log.info(f'start_date: {self.start_date}')
@@ -41,40 +82,40 @@ class PingNewsApiOperator(SimpleHttpOperator):
         return super().execute(context)
 
 
-class CrawlerOperator(SimpleHttpOperator):
-    query = 'bitcoin'
-
-    def execute(self, context):
-        return super().execute(context)
-
-
 def _response_check(response: requests.Response):
     log.info(f'response_code: {response.status_code}')
-    if response.status_code:
+
+    data = None
+    try:
+        data = response.json()
+    except:
+        pass
+
+    data = data or {}
+
+    if response.status_code == 200:
         return True
+    else:
+        err_code = data.get('code')
+        err_message = data.get('message')
+
+        log.error(f'{err_code} {err_message}')
+        raise NewsApiError()
 
 
 #
-news_bitcoin_task = CrawlerOperator(
-    task_id='news_bitcoin',
-    dag=newsapi_crawler_dag,
-    method='GET',
-    headers={'Authorization': f'Bearer {NEWSAPI_TOKEN}'},
-    response_check=_response_check,
-    log_response=False,
-    http_conn_id='newasapi',
-    endpoint='/'
-)
+ping_task = PingNewsApiOperator()
+get_schemas_task = GetDataSchemas()
+check_schemas_task = CheckDataSchemas()
+get_data_task = GetData()
+store_data_task = StoreDataToGoogleDrive()
+loads_data_task = LoadsDataToBigQuery()
+publish_data_task = PublishEventsToPubSub()
 
-# upstreams
-ping_task = PingNewsApiOperator(
-    task_id='ping_newasapi',
-    dag=newsapi_crawler_dag,
-    method='GET',
-    headers={'Authorization': f'Bearer {NEWSAPI_TOKEN}'},
-    response_check=_response_check,
-    log_response=False,
-    http_conn_id='newasapi',
-    endpoint='/'
-)
-news_bitcoin_task.set_upstream(ping_task)
+#
+publish_data_task.set_upstream(loads_data_task)
+loads_data_task.set_upstream(store_data_task)
+store_data_task.set_upstream(get_data_task)
+get_data_task.set_upstream(check_schemas_task)
+check_schemas_task.set_upstream(get_schemas_task)
+get_schemas_task.set_upstream(ping_task)
