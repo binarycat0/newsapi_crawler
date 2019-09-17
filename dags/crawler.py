67b785edfec23cf98cff965d22e05e0af664797f
Artur Rakhmatulin <arahmatulin@b2broker.net>
2019-09-17 12:57:15 +0300

    get source and articles schemas store source and articles schemas compare source and articles schemas

--- dags/crawler.py
+++ dags/crawler.py
@@ -1,7 +1,9 @@
+import abc
 import json
 import os
 import time
 from datetime import datetime, timedelta
+from distutils.util import strtobool
 
 import redis
 import requests
@@ -18,6 +20,7 @@ from newsapi import NewsApiClient
 from pymongo import MongoClient
 
 NEWSAPI_TOKEN = open(os.environ.get('NEWSAPI_TOKEN_FILE'), 'r').read()
+NEWSAPI_DEV_MODE = strtobool(os.environ.get('NEWSAPI_DEV_MODE'))
 
 MONGO_HOST = os.environ.get('MONGO_HOST')
 MONGO_PORT = int(os.environ.get('MONGO_PORT'))
@@ -27,7 +30,7 @@ MONGO_USER = os.environ.get('MONGO_USER')
 REDIS_HOST = os.environ.get('REDIS_HOST')
 REDIS_PORT = int(os.environ.get('REDIS_PORT'))
 
-GOOGLE_CLOUD_PROJECT = os.environ.get('GOOGLE_CLOUD_PROJECT')
+GOOGLE_CLOUD_PROJECT = open(os.environ.get('GOOGLE_CLOUD_PROJECT_FILE'), 'r').read()
 GOOGLE_APPLICATION_CREDENTIALS = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
 
 GOOGLE_CLOUD_TIMEOUT = int(os.environ.get('GOOGLE_CLOUD_TIMEOUT'))
@@ -40,6 +43,8 @@ MONGO_URI = f'mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}'
 log.debug(f'NEWSAPI_TOKEN: {NEWSAPI_TOKEN}')
 
 COLLECTION_SOURCES_KEY = 'sources'
+SOURCES_SCHEMA_SOURCES_KEY = 'sources-schema'
+ARTICLES_SCHEMA_SOURCES_KEY = 'articles-schema'
 GOOGLE_TIMEOUT_KEY = 'GOOGLE_TIMEOUT'
 
 # start date
@@ -48,7 +53,7 @@ start_date = datetime(_now.year, _now.month, _now.day)
 
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': False,
+    'depends_on_past': True,
     'start_date': start_date - timedelta(days=2),
     'email_on_failure': False,
     'email_on_retry': False,
@@ -126,8 +131,25 @@ def get_google_store_source_key(date: str):
     return f'{COLLECTION_SOURCES_KEY}-{date}'
 
 
+def get_google_store_source_schema_key(date: str):
+    return f'source-schema-{date}'
+
+
+def get_google_store_article_schema_key(date: str):
+    return f'article-schema-{date}'
+
+
 def get_newsapi_client() -> NewsApiClient:
     return NewsApiClient(NEWSAPI_TOKEN)
+
+
+def check_pubsub_topic(pubsub_client: pubsub_v1.PublisherClient, topic):
+    # check topic in pubsub
+    project_path = pubsub_client.project_path(GOOGLE_CLOUD_PROJECT)
+    topics = [topic_ref.name for topic_ref in pubsub_client.list_topics(project_path)]
+    if topic not in topics:
+        log.info(f'create pubsub topic {topic}')
+        pubsub_client.create_topic(topic)
 
 
 class NewsApiError(AirflowException): ...
@@ -171,10 +193,6 @@ class LoadsDataToBigQuery(_SimpleHttpOperator):
     task_id = 'loads_data_to_big_query'
 
 
-class StoreDataToGoogleDrive(_SimpleHttpOperator):
-    task_id = 'store_data_to_google_drive'
-
-
 class GetData(_SimpleHttpOperator):
     task_id = 'get_data_api'
 
@@ -188,35 +206,38 @@ class GetData(_SimpleHttpOperator):
 class _BaseOperator(BaseOperator):
     task_id: str
     execution_date: datetime
+    prev_execution_date: datetime
 
     def __init__(self, *args, **kwargs):
         super().__init__(task_id=self.task_id, dag=default_dag, *args, **kwargs)
 
     def execute(self, context: dict):
         setattr(self, 'execution_date', context.get('execution_date'))
+        setattr(self, 'prev_execution_date', (context.get('execution_date') - timedelta(days=1)))
+
+        return self.execute_(context)
+
+    @abc.abstractmethod
+    def execute_(self, context: dict):
+        pass
 
 
 class CheckAndPublishNewsSources(_BaseOperator):
     task_id = 'publish_news_sources'
 
-    def execute(self, context: dict):
-        super().execute(context)
-
+    def execute_(self, context: dict):
         # get pubsub client
         pubsub_client = get_publisher_client()
         topic = pubsub_client.topic_path(GOOGLE_CLOUD_PROJECT, COLLECTION_SOURCES_KEY)
 
         # check topic in pubsub
-        topics = [topic_ref.name for
-                  topic_ref in pubsub_client.list_topics(pubsub_client.project_path(GOOGLE_CLOUD_PROJECT))]
-        if topic not in topics:
-            pubsub_client.create_topic(topic)
+        check_pubsub_topic(pubsub_client, topic)
 
         # get sore client
         firestore_client = get_store_client()
 
         execution_date = self.execution_date.date().isoformat()
-        prev_execution_date = (self.execution_date.date() - timedelta(days=1)).isoformat()
+        prev_execution_date = self.prev_execution_date.isoformat()
 
         # get previous news_sources
         current_sources_ref = firestore_client.collection(get_google_store_source_key(execution_date))
@@ -234,9 +255,7 @@ class CheckAndPublishNewsSources(_BaseOperator):
 class GetNewsSources(_BaseOperator):
     task_id = 'get_news_sources'
 
-    def execute(self, context: dict):
-        super().execute(context)
-
+    def execute_(self, context: dict):
         newsapi_client = get_newsapi_client()
         sources = newsapi_client.get_sources()['sources']
 
@@ -268,22 +287,96 @@ publish_news_sources.set_upstream(get_news_sources)
 ############
 
 
-class CheckDataSchemas(_SimpleHttpOperator):
+class CheckDataSchemas(_BaseOperator):
     task_id = 'check_data_schemas'
 
+    def execute_(self, context):
+        execution_date = self.execution_date.date().isoformat()
+        prev_execution_date = self.prev_execution_date.isoformat()
 
-class GetDataSchemas(_SimpleHttpOperator):
+        firestore_client = get_store_client()
+
+        sources_schema_ref = firestore_client.collection('sources_schema')
+        article_schema_ref = firestore_client.collection('article_schema')
+
+        # get pubsub client
+        pubsub_client = get_publisher_client()
+        sources_schema_topic = pubsub_client.topic_path(GOOGLE_CLOUD_PROJECT, SOURCES_SCHEMA_SOURCES_KEY)
+        articles_schema_topic = pubsub_client.topic_path(GOOGLE_CLOUD_PROJECT, ARTICLES_SCHEMA_SOURCES_KEY)
+
+        # check topic in pubsub
+        check_pubsub_topic(pubsub_client, sources_schema_topic)
+        check_pubsub_topic(pubsub_client, articles_schema_topic)
+
+        # source schema
+        prev_sources_schema = sources_schema_ref.document(prev_execution_date).get()
+        sources_schema = sources_schema_ref.document(execution_date).get()
+
+        if prev_sources_schema.exists and sources_schema.exists:
+
+            remove = set(prev_sources_schema.to_dict()['keys']).difference(set(sources_schema.to_dict()['keys']))
+            new = set(sources_schema.to_dict()['keys']).difference(set(prev_sources_schema.to_dict()['keys']))
+
+            if len(remove):
+                pubsub_client.publish(sources_schema_topic, bytes(json.dumps({'remove': list(remove)}), 'utf8'))
+            if len(new):
+                pubsub_client.publish(sources_schema_topic, bytes(json.dumps({'new': list(new)}), 'utf8'))
+        elif sources_schema.exists:
+            pubsub_client.publish(
+                articles_schema_topic,
+                bytes(json.dumps({'new': list(sources_schema.to_dict()['keys'])}), 'utf8')
+            )
+
+        # article schema
+        prev_article_schema = article_schema_ref.document(prev_execution_date).get()
+        article_schema = article_schema_ref.document(execution_date).get()
+
+        if prev_article_schema.exists and article_schema.exists:
+
+            remove = set(prev_article_schema.to_dict()['keys']).difference(set(article_schema.to_dict()['keys']))
+            new = set(article_schema.to_dict()['keys']).difference(set(prev_article_schema.to_dict()['keys']))
+
+            if len(remove):
+                pubsub_client.publish(articles_schema_topic, bytes(json.dumps({'remove': list(remove)}), 'utf8'))
+            if len(new):
+                pubsub_client.publish(articles_schema_topic, bytes(json.dumps({'new': list(new)}), 'utf8'))
+        elif article_schema.exists:
+            pubsub_client.publish(
+                articles_schema_topic,
+                bytes(json.dumps({'new': list(article_schema.to_dict()['keys'])}), 'utf8')
+            )
+
+
+class GetDataSchemas(_BaseOperator):
     task_id = 'get_data_schemas'
 
+    def execute_(self, context):
+        execution_date = self.execution_date.date().isoformat()
 
-class PingNewsApiOperator(_SimpleHttpOperator):
+        client = get_newsapi_client()
+        firestore_client = get_store_client()
+
+        sources = client.get_sources(language='en').get('sources')
+        top_headlines = client.get_top_headlines(page_size=1, page=1).get('articles')
+
+        source = sources.pop()
+        headline = top_headlines.pop()
+
+        source_keys = list(source.keys())
+        headline_keys = list(headline.keys())
+
+        sources_schema_ref = firestore_client.collection('sources_schema')
+        article_schema_ref = firestore_client.collection('article_schema')
+
+        sources_schema_ref.document(execution_date).set({'keys': source_keys})
+        article_schema_ref.document(execution_date).set({'keys': headline_keys})
+
+
+class PingNewsApiOperator(_BaseOperator):
     task_id = 'ping_news_api'
 
-    def execute(self, context):
-        log.info(f'start_date: {self.start_date}')
-        log.info(f'end_date: {self.end_date}')
-        log.info(f'execution_date: {context.get("execution_date")}')
-        return super().execute(context)
+    def execute_(self, context):
+        get_newsapi_client()
 
 
 #
@@ -291,14 +384,12 @@ ping_task = PingNewsApiOperator()
 get_schemas_task = GetDataSchemas()
 check_schemas_task = CheckDataSchemas()
 get_data_task = GetData()
-store_data_task = StoreDataToGoogleDrive()
 loads_data_task = LoadsDataToBigQuery()
 publish_data_task = PublishEventsToPubSub()
 
 #
 publish_data_task.set_upstream(loads_data_task)
-loads_data_task.set_upstream(store_data_task)
-store_data_task.set_upstream(get_data_task)
+loads_data_task.set_upstream(get_data_task)
 get_data_task.set_upstream(check_schemas_task)
 get_data_task.set_upstream(publish_news_sources)
 
