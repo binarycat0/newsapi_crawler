4e0e870dce5e2b98bf06c47b9a0f492981ee0bbd
Artur Rakhmatulin <arahmatulin@b2broker.net>
2019-09-18 14:39:02 +0300

    fix entrypoints

--- entrypoint/scheduler.sh
+++ entrypoint/scheduler.sh
@@ -2,10 +2,14 @@
 airflow upgradedb && \
 airflow connections -a --conn_id=newsapi --conn_uri=https://newsapi.org/
 
+rm -rf $AIRFLOW_HOME/airflow-flower.pid
+rm -rf $AIRFLOW_HOME/airflow-scheduler.pid
+rm -rf $AIRFLOW_HOME/airflow-worker.pid
+
 set -m
 
-airflow flower -p 8081 &
-airflow scheduler &
-airflow worker
+airflow flower --pid airflow-flower.pid -p 8081 &
+airflow scheduler --pid airflow-scheduler.pid &
+airflow worker --pid airflow-worker.pid
 
 fg %1
--- entrypoint/web.sh
+++ entrypoint/web.sh
@@ -4,4 +4,4 @@ airflow connections -a --conn_id=newsapi --conn_uri=https://newsapi.org/
 
 rm -rf $AIRFLOW_HOME/airflow-webserver.pid
 
-airflow webserver -p 8080
+airflow webserver --pid airflow-webserver.pid -p 8080
